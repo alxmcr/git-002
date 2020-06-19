0e698a6d12dcbaf6258e2a799232e0cb5170d0c7
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-06-19 17:05:03 -0400

    + New function qestions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 function hola(name) { console.log("Hola " + name); }
 function bye(name){ console.log("bye..."); }
 function incompleta(){ console.log("incompleta"); }
+function question(name){ console.log("I have a question..." + name) }
